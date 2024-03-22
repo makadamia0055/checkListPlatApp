61ceee9036c0c18b7a48e4dda79e33ef99c323ed
makadamia0055 <makend91@naver.com>
2024-03-22 17:41:30 +0900

    project init

---
+++ src/main/java/com/checklist/checklistplatformapp/CheckListPlatformAppApplication.java
@@ -0,0 +1,13 @@
+package com.checklist.checklistplatformapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckListPlatformAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckListPlatformAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/checklist/checklistplatformapp/CheckListPlatformAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checklist.checklistplatformapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckListPlatformAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
